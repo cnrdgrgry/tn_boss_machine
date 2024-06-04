bce75182a3bd647b30f41f3ad64cd72eaf7bd071
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-06-04 14:32:08 +0100

    Add Get and Post routes to minionRoutes

--- server/minionRoutes.js
+++ server/minionRoutes.js
@@ -4,15 +4,31 @@
 const express = require('express');
 const router = express.Router()
 
+//requiring database + helperfunctions.
+
+const { getAllFromDatabase, addToDatabase } = require('db.js');
+
 //defining the routes
 
-router.get('/', (req, res)
+router.get('/', (req, res ) => {
 //get an array of all
-);
+const minions = getAllFromDatabase('minions');
+if (minions) {
+    res.status(200).json(minions);
+    } else {
+    res.status(404).json({error: 'Error 404; Resource not found!'})
+    };
+});
 
-router.post("/", (req, res)
+router.post("/", (req, res) => {
 //create a new minion and save to the db
-);
+const addMinion = addToDatabase('minions', req.body) 
+    if (addMinion) {
+        res.status(200).json(addMinion);
+    } else {
+        res.status(500).json({error: 'Error 500: Error making minion!'})
+    }
+});
 
 router.get("/:minionId", (req, res)
     const { minionId } = req.params;
